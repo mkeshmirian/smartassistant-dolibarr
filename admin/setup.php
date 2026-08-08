d266ba1a26ef16702ffbb2f61b4581992f0620a6
mkeshmirian <mkeshmirian@yahoo.com>
2026-08-07 17:03:09 -0700

    v1.0.10: top-bar icon update (square full-bleed icon-white-short3)

--- admin/setup.php
+++ admin/setup.php
@@ -64,7 +64,7 @@ if ($action == 'save') {
 		$payload = json_encode(array(
 			'dolibarr_url' => $dolibarrUrl,
 			'api_key' => $dolibarrApiKey,
-			'module_version' => '1.0.9',
+			'module_version' => '1.0.10',
 			'dolibarr_version' => DOL_VERSION,
 			'entity' => $conf->entity,
 		));
